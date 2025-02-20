b3325063a8585aa0aa2474479b9ed3fefe7197a4
CauZy-Goes <cauafariasdev@gmail.com>
2025-02-20 17:25:56 -0300

    feat: add new dtos

---
+++ src/main/java/io/github/cauzy/GSDS/DTO/TipoSalaDTO.java
@@ -0,0 +1,22 @@
+package io.github.cauzy.GSDS.DTO;
+
+public class TipoSalaDTO {
+    private Integer idTipoSala;
+    private String nomeSala;
+
+    public Integer getIdTipoSala() {
+        return idTipoSala;
+    }
+
+    public void setIdTipoSala(Integer idTipoSala) {
+        this.idTipoSala = idTipoSala;
+    }
+
+    public String getNomeSala() {
+        return nomeSala;
+    }
+
+    public void setNomeSala(String nomeSala) {
+        this.nomeSala = nomeSala;
+    }
+}

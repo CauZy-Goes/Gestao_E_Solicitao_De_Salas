8bec4b720b0a0322e81b7a9c07e7432e05afc2c0
CauZy-Goes <cauafariasdev@gmail.com>
2025-02-22 11:27:04 -0300

    feat: add solicitacoesAvaliadorMB

---
+++ src/main/java/io/github/cauzy/GSDS/Controller/SolicitacoesAvaliadorMB.java
@@ -0,0 +1,115 @@
+package io.github.cauzy.GSDS.Controller;
+
+import io.github.cauzy.GSDS.Client.SolicitacaoClient;
+import io.github.cauzy.GSDS.DTO.SolicitacaoDTO;
+import io.github.cauzy.GSDS.DTO.UsuarioDTO;
+import io.github.cauzy.GSDS.Utility.Exception.EntityCreationException;
+import io.github.cauzy.GSDS.Utility.Exception.EntityNotFoundException;
+import io.github.cauzy.GSDS.Utility.Message;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Named
+@ViewScoped
+public class SolicitacoesAvaliadorMB implements Serializable {
+
+    @Inject
+    SolicitacaoClient solicitacaoClient;
+
+    SolicitacaoDTO solicitacaoDTO = new SolicitacaoDTO();
+
+    List<SolicitacaoDTO> solicitacaoDTOList;
+
+
+//    1 = pendente, 2 = Aceita, 3 = Rejeitada
+    Integer status;
+
+    @PostConstruct
+    public void init() {
+        try {
+            solicitacaoDTOList = solicitacaoClient.listarSolicitacoes();
+        } catch (EntityNotFoundException e) {
+            Message.erro("Erro ao carregar solicitacao: " + e.getMessage());
+        }
+    }
+
+    //    1 = pendente, 2 = Aceita, 3 = Rejeitada
+
+    public void aceitarSolicitacao(Integer id) throws EntityCreationException {
+        solicitacaoDTO.setIdStatus(2);
+        solicitacaoDTO.setDataHoraAprovacao(LocalDateTime.now());
+        solicitacaoDTO.setIdUsuarioAvaliador(getAvaliadorId());
+        solicitacaoClient.updateSolicitacao(solicitacaoDTO, id);
+    }
+
+    public void rejeitarSolicitacao(Integer id) throws EntityCreationException {
+        solicitacaoDTO.setIdStatus(3);
+        solicitacaoDTO.setDataHoraAprovacao(LocalDateTime.now());
+        solicitacaoDTO.setIdUsuarioAvaliador(getAvaliadorId());
+        solicitacaoClient.updateSolicitacao(solicitacaoDTO, id);
+    }
+
+    public Integer getAvaliadorId(){
+        UsuarioDTO professor = (UsuarioDTO) getSession().getAttribute("gestor");
+        return  professor.getIdUsuario();
+    }
+
+    public HttpSession getSession(){
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        return  (HttpSession) externalContext.getSession(false);
+    }
+
+    public String dataHoraFormatada(LocalDateTime dataHora){
+        if (dataHora == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm  dd/MM/yyyy");
+        return dataHora.format(formatter);
+    }
+
+    private Integer selectedStatus;
+
+    public void filterSolicitacoes() {
+        if (selectedStatus != null) {
+            solicitacaoDTOList = solicitacaoDTOList.stream()
+                    .filter(s -> s.getIdStatus().equals(selectedStatus))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public SolicitacaoClient getSolicitacaoClient() {
+        return solicitacaoClient;
+    }
+
+    public SolicitacaoDTO getSolicitacaoDTO() {
+        return solicitacaoDTO;
+    }
+
+    public void setSolicitacaoDTO(SolicitacaoDTO solicitacaoDTO) {
+        this.solicitacaoDTO = solicitacaoDTO;
+    }
+
+    public List<SolicitacaoDTO> getSolicitacaoDTOList() {
+        return solicitacaoDTOList;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
